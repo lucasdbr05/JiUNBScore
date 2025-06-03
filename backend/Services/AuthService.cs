17b5ff122857ccee2de6f80db3c1f6e0840d642a
Lucas Lima <lucasgabrieljake@gmail.com>
2025-06-03 18:42:23 -0300

    feat: auth service

---
+++ backend/Services/AuthService.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using Backend.Contexts;
+using Backend.ViewModels;
+
+namespace Backend.Services;
+
+public class AuthService
+{
+    private readonly JiunbDBContext _context;
+    private readonly IConfiguration _config;
+
+    public AuthService(JiunbDBContext dBContext, IConfiguration config)
+    {
+        _context = dBContext;
+        _config = config;
+    }
+
+    public string Login(LoginViewModel data)
+    {
+        var user = _context.Users
+            .FromSql($"SELECT * FROM Usuario WHERE nickname = '{data.Name}'")
+            .FirstOrDefault();
+
+        if (user == null || user.Password != data.Password)
+            return null;
+
+        return GenerateJwtToken(user.Nickname);
+    }
+
+    private string GenerateJwtToken(string userId)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(_config["Jwt:Key"]);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId)
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(double.Parse(_config["Jwt:ExpiresInMinutes"])),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
