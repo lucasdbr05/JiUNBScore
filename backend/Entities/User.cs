a4454fc6fac15bc086503d09d49c79219a70edb7
Lucas Lima <lucasgabrieljake@gmail.com>
2025-06-03 18:42:11 -0300

    feat: user entity

---
+++ backend/Entities/User.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Entities;
+
+[Table("Usuario")]
+public class User
+{
+    public User(string nickname, string password, string email)
+    {
+        Nickname = nickname;
+        Email = email;
+        Password = password;
+    }
+
+    [Key]
+    [Column("nickname")]
+    public string Nickname { get; set; }
+
+    [Column("email")]
+    public string Email { get; set; }
+
+    [Column("senha")]
+    public string Password { get; set; }
+}
