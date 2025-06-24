9247d37afff347c3607903e7f159f93e33c85849
Lucas Lima <lucasgabrieljake@gmail.com>
2025-06-24 13:25:28 -0300

    hotfix: minor changes

--- Backend/ViewModels/Edition/CreateEditionViewModel.cs
+++ Backend/ViewModels/Edition/CreateEditionViewModel.cs
@@ -8,7 +8,7 @@ public class CreateEditionViewModel
         DateTime dataComeco
     )
     {
-        DataComeco = dateComeco;
+        DataComeco = dataComeco;
         DataFim = dataFim;
     }
 
