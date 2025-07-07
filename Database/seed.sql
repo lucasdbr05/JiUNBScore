9cfb65ffa45768e1fb3053c767256ec24b4ab1be
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-07 18:47:01 -0300

    feat: DATETIME on partidas date

--- Database/seed.sql
+++ Database/seed.sql
@@ -29,7 +29,7 @@ INSERT INTO Local (ra, cep, quadra, rua, lote)
 VALUES ('Asa Norte', '70855-530', 'Cidade Universitária', 'Rua Santander', 'Arena Zhou');
 
 INSERT INTO Partidas (placar_time_1, placar_time_2, id_edicao, id_fase, id_local, id_time_1, id_time_2, date)
-VALUES (4, 0, 1, 1, 1, 1, 2, '2025-11-04');
+VALUES (4, 0, 1, 1, 1, 1, 2, '2025-10-04T19:00:00');
 
 INSERT INTO Acao (nome, pontuacao, id_esporte)
 VALUES ('Gol', 100, 1),
@@ -46,5 +46,5 @@ VALUES (1, 1),
        (1, 3),
        (1, 4);
 
-INSERT INTO EsportesEdicao (id_edicao, id_esporte) 
+INSERT INTO EsporteEdicao (id_edicao, id_esporte) 
 VALUES (1, 1);
